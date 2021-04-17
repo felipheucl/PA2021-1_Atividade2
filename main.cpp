ec847cd58675dbe9840ad0f78c79a5d3e3529170
Feliphe Rodrigues dos Santos <>
2021-04-17 12:25:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+/*
+⦁	2: a, b, c
+⦁	3: d, e, f
+⦁	4: g, h, i
+⦁	5: j, k, l
+⦁	6: m, n, o
+⦁	7: p, q, r, s
+⦁	8: t, u, v
+⦁	9: w, x, y, z
+
+ex. p: #7=1
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+
+int main (){
+ifstream ler;
+char   alvo,
+       cod1[]={'a','d','g','j','m','p','t','w'},
+       cod2[]={'b','e','h','k','n','q','u','x'},
+       cod3[]={'c','f','i','l','o','r','v','y'},
+       cod4[]={' ',' ',' ',' ',' ','s',' ','z'};
+string cod5[]={"#2=","#3=","#4=","#5=","#6=","#7=","#8=","#9="};
+char palavra[50];
+int apertar=0,tecla=0,ind=0;
+
+
+cout << "********************************************" << endl<< endl;
+cout << "ATIVIDADE 2 - Problema da escrita no celular" << endl<< endl;
+cout << "Digite uma palavra: (maximo de 50 caracteres) " << endl<< endl;
+cin.getline(palavra,50);
+cout << "Segue a codificacao para: " << palavra << endl<< endl;
+ 
+for(int i=0; i<strlen(palavra); i++)
+  {
+    alvo=palavra[i];
+    for(ind=0; ind<=7; ind++)
+    {   
+        if (alvo==cod1[ind])
+            {apertar=1;
+             tecla=ind;           
+            }
+        if (alvo==cod2[ind])
+            {apertar=2;  
+             tecla=ind;          
+            }
+        if (alvo==cod3[ind])
+            {apertar=3;
+             tecla=ind;           
+            }
+        if (alvo==cod4[ind])
+            {apertar=4;
+             tecla=ind;           
+            }
+    }
+    
+    cout << cod5[tecla]<<apertar<<endl;
+    
+  }
+
+ 
+  /*
+  cout << "Deseja consultar mais uma palavra? 1-SIM ou 2-NÃO\n";
+  cin>>escolha;
+  if(escolha==1)
+  */
+}
